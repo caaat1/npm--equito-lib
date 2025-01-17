1f6803d485d39f0a65d666027ac9142e43901868
caaat1 <kgolov@hotmail.com>
2025-01-17 18:14:34 +0300

    Before changing the class structure to straightforward the method invocation

---
+++ src/types/index.d.ts
@@ -0,0 +1,14 @@
+// import * as Console from '../Console';
+// import LocalNews from '../LocalNews';
+// import './types';
+
+// declare module 'equito-library' {
+//   export class Col extends LocalNews {
+//     protected out(...args: Console.T[]): void;
+//   }
+//   export class Row extends LocalNews {
+//     protected out(...args: Console.T[]): void;
+//   }
+// }
+// export const Col: typeof import('../LocalNews/Col').default;
+// export const Row: typeof import('../LocalNews/Row').default;
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,10 +1,10 @@
-declare namespace Console {
-  type T = unknown;
-  const Log: {
-    col: (...args: T[]) => void;
-    row: (...args: T[]) => void;
-  };
-}
-declare class LocalNews {
-  protected out(...args: unknown[]): void;
-}
+// declare namespace Console {
+//   type T = unknown;
+//   const Log: {
+//     col: (...args: T[]) => void;
+//     row: (...args: T[]) => void;
+//   };
+// }
+// declare class LocalNews {
+//   protected out(...args: unknown[]): void;
+// }
